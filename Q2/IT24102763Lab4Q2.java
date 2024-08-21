708c0a64b8cc73d3e452a352eb97d83e22455c11
IT24102763 <it24102763@my.sliit.lk>
2024-08-21 09:17:11 +0530

    Add  IT24102763Lab4Q2.java to Q2

--- Q2/IT24102763Lab4Q2.java
+++ Q2/IT24102763Lab4Q2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ITxxxxxxxxLab4Q2 {
+public class IT24102763Lab4Q2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
